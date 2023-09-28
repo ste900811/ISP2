fc0dbcd44e6940f349587ae2f06b615fc95ecdc2
Yiyang Lu <122488816+ste900811@users.noreply.github.com>
2023-09-27 22:53:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function generate() {
+    if(myWindow != null) {
+        myWindow.close();
+    }
+
+    // Generate the website title
+    let firstName = document.getElementById("firstName").value;
+    if (!firstName){
+        alert("Please Enter First Name");
+        return;
+    }
+    let lastName = document.getElementById("lastName").value;
+    if (!lastName){
+        alert("Please Enter Last Name");
+        return;
+    }
+    let date = new Date().toLocaleDateString("de-DE");
+    console.log(date);
+    page += "<h1>Generate by " + firstName + " " + lastName + " on " + date + "</h1>";
+    page += "<br>";
+
+    // Generate the Website Links
+    for (let i = 0; i < websiteCount; i++) {
+        var websiteName = document.getElementById("websiteName" + i).value;
+        var websiteAddress = document.getElementById("websiteAddress" + i).value;
+        page += "<button onclick='window.open(" + "\"http://" + websiteAddress + "\")'>" + websiteName + "</button>";
+    }
+
+    // Create new window and open with "page"
+    myWindow = window.open("", "ISP", "width=800,height=168");
+    myWindow.document.write(page);
+}
+    
+function addWebsite() {
+    let table = document.getElementById("Websites");
+    let row = table.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    cell1.innerHTML = "<input type=\"text\" id=\"websiteName" + websiteCount + "\" style=\"width: 50%;\">";
+    cell2.innerHTML = "<input type=\"text\" id=\"websiteAddress" + websiteCount + "\" style=\"width: 50%;\">";
+    websiteCount += 1;
+}
+
+function deleteWebsite() {
+    if (websiteCount == 1) {
+        alert("You can't delete more row!");
+        return;
+    }
+    document.getElementById("Websites").deleteRow(-1);
+    websiteCount -= 1;
+}
