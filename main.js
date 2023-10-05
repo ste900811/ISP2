5b16fcb942b2af17d0181bb41b59d0b64d4f12f6
Yiyang Lu <ste900811@gmail.com>
2023-10-04 21:37:19 -0400

    Add drag and drop to animal section

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Generate the webpage
 function generate() {
     let animalX = 10;
-    console.log("check1", typeof(animalX) , animalX);
-    let animalY = 0;
+    let animalY = 10;
     if(myWindow != null) {
         myWindow.close();
+        myWindow = null;
+        animalX = 10;
+        animalY = 10;
         page = "";
     }
 
@@ -26,44 +28,40 @@ function generate() {
     let colorBlue = document.getElementById("colorBlue").value;
     let date = new Date().toLocaleDateString("de-DE");
     animalX = parseFloat(animalX) + parseFloat(fontsize);
-    console.log("check2", typeof(animalX) , animalX);
     page += "<body><h1 style=\"font-size:" + fontsize + "px; font-family:" + fontStyle + "; color:rgb(" + colorRed + "," + colorGreen + "," + colorBlue + ");\"> Generate by " + firstName + " " + lastName + " on " + date + "</h1>";
     animalX = parseFloat(animalX) + 21;
-    console.log("check3", typeof(animalX) , animalX);
     page += "<br>";
 
     // Generate the button for Website Links
-    let buttonCount = 0;
     for (let i = 0; i < websiteCount; i++) {
         var websiteName = document.getElementById("websiteName" + i).value;
         var websiteAddress = document.getElementById("websiteAddress" + i).value;
         page += "<button style=\"margin-right:10px;\" onclick='window.open(" + "\"http://" + websiteAddress + "\")'>" + websiteName + "</button>";
-        buttonCount += 1;
-        if (buttonCount%5 === 0) {
+        if ((i+1)%5 === 0) {
             animalX = parseFloat(animalX) + 21;
-            console.log("check4", typeof(animalX) , animalX);
             page += "<br>";
         }
     }
     animalX = parseFloat(animalX) + 21;
-    console.log("check5", typeof(animalX) , animalX);
     page += "<br>";
 
-    // Generate drag and drop text and function
-    page += "<p style = \"font-size: 25px; position: absolute; top:" + animalX + "px; left: 0px; background-color: lightgrey;\" onmousedown = \"grabber(event);\"> !!DRAG AND MOVE AWAY!!This is ISP PA2</p>";
-    page += "<script>var diffX,diffY,theElement;function grabber(event){theElement=event.currentTarget;let posX=parseInt(theElement.style.left);let posY = parseInt(theElement.style.top);"
-    page += "diffX=event.clientX-posX;diffY=event.clientY-posY;document.addEventListener(\"mousemove\",mover,true);document.addEventListener(\"mouseup\", dropper, true);event.stopPropagation();event.preventDefault();}"
-    page += "function mover(event){theElement.style.left=(event.clientX-diffX)+\"px\";theElement.style.top=(event.clientY-diffY)+\"px\";event.stopPropagation();}function dropper(event){document.removeEventListener(\"mouseup\""
-    page += ", dropper, true);document.removeEventListener(\"mousemove\", mover, true);event.stopPropagation();}</script></body>"
+    // Generate drag and drop text
+    page += "<p style = \"font-size: 25px; position: absolute; top:" + animalX + "px; left: 0px; background-color: lightgrey;\" onmousedown = \"grabber(event);\"> !!DRAG AND DROP THE ANIMAL!!</p>";
     animalX = parseFloat(animalX) + 25;
     
     // Generate the animal section
+    animalX = parseFloat(animalX) + 30;
     let animals = document.getElementsByName("animal");
-    let animalsCount = 0;
+    let animalCount = 0;
     for (let i = 0; i < animals.length; i++) {
-        if (animals[i].checked){
-            page += "<div class=\"animals\" style=\"width:120px; height:120px; float:left; margin: 10px;\" onclick=\"playSound('" + animals[i].id + "')\"><img src=\"./Pictures/" + animals[i].id + ".png\" width=\"120px\"></div>";
-            animalsCount += 1;
+        if (animals[i].checked) {
+            page += "<div class=\"animals\" style=\" height:120px; width:auto; position: absolute; top:" + animalX + "px; left:" + animalY + "px;\" onmousedown = \"grabber(event)\"; onclick=\"playSound('" + animals[i].id + "')\"><img src=\"./Pictures/" + animals[i].id + ".png\" width=\"120px\"></div>";
+            animalY = parseFloat(animalY) + 130; 
+            animalCount += 1;
+            if (animalCount%5 === 0) {
+                animalX = parseFloat(animalX) + 130;
+                animalY = 10;
+            }
         }
     }
     page += "<script>function playSound(animal){let audio = new Audio(\"./Sounds/\" + animal + \".wav\");audio.play();}</script>";
@@ -75,6 +73,12 @@ function generate() {
             page += "<style>body{background-image:url(\"./Pictures/BackGrounds/" + backGrounds[i].id + ".jpg\");background-size:cover;background-color:hsla(0, 100%, 30%, opacity);}</style>"
         }
     }
+
+    // Generate the script
+    page += "<script>var diffX,diffY,theElement;function grabber(event){theElement=event.currentTarget;let posX=parseInt(theElement.style.left);let posY = parseInt(theElement.style.top);"
+    page += "diffX=event.clientX-posX;diffY=event.clientY-posY;document.addEventListener(\"mousemove\",mover,true);document.addEventListener(\"mouseup\", dropper, true);event.stopPropagation();event.preventDefault();}"
+    page += "function mover(event){theElement.style.left=(event.clientX-diffX)+\"px\";theElement.style.top=(event.clientY-diffY)+\"px\";event.stopPropagation();}function dropper(event){document.removeEventListener(\"mouseup\""
+    page += ", dropper, true);document.removeEventListener(\"mousemove\", mover, true);event.stopPropagation();}</script></body>"
 
     // Create new window and open with "page"
     myWindow = window.open("", "ISP", "width=800, height=1200");
